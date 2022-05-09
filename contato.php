a954d1b302b853801b71793f29d103a977054753
Ino-Codes <103116915+Ino-Codes@users.noreply.github.com>
2022-05-09 09:51:39 -0300

    Home pré-final
    exclusão e adição de elementos na Home conforme elaborado no protótipo.

---
+++ contato.php
@@ -0,0 +1,181 @@
+    <!-- Team section Start -->
+    <section id="team" class="section">
+        <div class="container">
+          <div class="section-header">          
+            <h2 class="section-title">Our Team</h2>
+            <span>Team</span>
+            <p class="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos debitis.</p>
+          </div>
+          <div class="row">
+            <div class="col-lg-3 col-md-6 col-xs-12">
+              <div class="single-team">
+                <img src="img/team/team1.png" alt="">
+                <div class="team-details">
+                  <div class="team-inner">
+                    <h4 class="team-title">Jhon Doe</h4>
+                    <p>Chief Technical Officer</p>
+                    <ul class="social-list">
+                      <li class="facebook"><a href="#"><i class="lni-facebook-filled"></i></a></li>
+                      <li class="twitter"><a href="#"><i class="lni-twitter-filled"></i></a></li>
+                      <li class="google-plus"><a href="#"><i class="lni-google-plus"></i></a></li>
+                      <li class="linkedin"><a href="#"><i class="lni-linkedin-fill"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-xs-12">
+              <div class="single-team">
+                <img src="img/team/team2.png" alt="">
+                <div class="team-details">
+                  <div class="team-inner">
+                    <h4 class="team-title">Luke Chesser</h4>
+                    <p>Marketing Executive</p>
+                    <ul class="social-list">
+                      <li class="facebook"><a href="#"><i class="lni-facebook-filled"></i></a></li>
+                      <li class="twitter"><a href="#"><i class="lni-twitter-filled"></i></a></li>
+                      <li class="google-plus"><a href="#"><i class="lni-google-plus"></i></a></li>
+                      <li class="linkedin"><a href="#"><i class="lni-linkedin-fill"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-xs-12">
+              <div class="single-team">
+                <img src="img/team/team3.png" alt="">
+                <div class="team-details">
+                  <div class="team-inner">                  
+                    <h4 class="team-title">David Givens</h4>
+                    <p>Business Manager</p>
+                    <ul class="social-list">
+                      <li class="facebook"><a href="#"><i class="lni-facebook-filled"></i></a></li>
+                      <li class="twitter"><a href="#"><i class="lni-twitter-filled"></i></a></li>
+                      <li class="google-plus"><a href="#"><i class="lni-google-plus"></i></a></li>
+                      <li class="linkedin"><a href="#"><i class="lni-linkedin-fill"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-xs-12">
+              <div class="single-team">
+                <img src="img/team/team4.png" alt="">
+                <div class="team-details">
+                  <div class="team-inner">
+                    <h4 class="team-title">Annie Spratt</h4>
+                    <p>Graphic Designer</p>
+                    <ul class="social-list">
+                      <li class="facebook"><a href="#"><i class="lni-facebook-filled"></i></a></li>
+                      <li class="twitter"><a href="#"><i class="lni-twitter-filled"></i></a></li>
+                      <li class="google-plus"><a href="#"><i class="lni-google-plus"></i></a></li>
+                      <li class="linkedin"><a href="#"><i class="lni-linkedin-fill"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Team section End -->
+  
+
+    <!-- Contact Section Start -->
+    <section id="contact" class="section">      
+        <div class="contact-form">
+          <div class="container">
+            <div class="section-header">          
+              <h2 class="section-title">Get In Touch</h2>
+              <span>Contact</span>
+              <p class="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos debitis.</p>
+            </div>
+            <div class="row">          
+              <div class="col-lg-9 col-md-9 col-xs-12">
+                <div class="contact-block">
+                  <form id="contactForm">
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <input type="text" class="form-control" id="name" name="name" placeholder="Your Name" required data-error="Please enter your name">
+                          <div class="help-block with-errors"></div>
+                        </div>                                 
+                      </div>
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <input type="text" placeholder="Your Email" id="email" class="form-control" name="name" required data-error="Please enter your email">
+                          <div class="help-block with-errors"></div>
+                        </div> 
+                      </div>
+                      <div class="col-md-12">
+                        <div class="form-group">
+                          <input type="text" placeholder="Subject" id="msg_subject" class="form-control" required data-error="Please enter your subject">
+                          <div class="help-block with-errors"></div>
+                        </div>
+                      </div>
+                      <div class="col-md-12">
+                        <div class="form-group"> 
+                          <textarea class="form-control" id="message" placeholder="Your Message" rows="7" data-error="Write your message" required></textarea>
+                          <div class="help-block with-errors"></div>
+                        </div>
+                        <div class="submit-button">
+                          <button class="btn btn-common btn-effect" id="submit" type="submit">Send Message</button>
+                          <div id="msgSubmit" class="h3 hidden"></div> 
+                          <div class="clearfix"></div> 
+                        </div>
+                      </div>
+                    </div>            
+                  </form>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-3 col-xs-12">
+                <div class="contact-deatils">
+                  <!-- Content Info -->
+                  <div class="contact-info_area">
+                    <div class="contact-info">
+                      <i class="lni-map"></i>
+                      <h5>Location</h5>
+                      <p>1900 Pico Blvd, New York br Centernial, colorado</p>
+                    </div>
+                    <!-- Content Info -->
+                    <div class="contact-info">
+                      <i class="lni-star"></i>
+                      <h5>E-mail</h5>
+                      <p>info@example.com</p>
+                    </div>
+                    <!-- Content Info -->
+                    <div class="contact-info">
+                      <i class="lni-phone"></i>
+                      <h5>Phone</h5>
+                      <p>+48 123 456 789</p>
+                    </div>
+                    <!-- Icon -->
+                    <ul class="footer-social">
+                      <li><a class="facebook" href="#"><i class="lni-facebook-filled"></i></a></li>
+                      <li><a class="twitter" href="#"><i class="lni-twitter-filled"></i></a></li>
+                      <li><a class="linkedin" href="#"><i class="lni-linkedin-fill"></i></a></li>
+                      <li><a class="google-plus" href="#"><i class="lni-google-plus"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>            
+      </section>
+      <!-- Contact Section End -->
+  
+
+
+          <!-- Map Section Start -->
+    <section id="google-map-area">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12 padding-0">
+              <object style="border:0; height: 450px; width: 100%;" data="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d34015.943594576835!2d-106.43242624069771!3d31.677719472407432!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86e75d90e99d597b%3A0x6cd3eb9a9fcd23f1!2sCourtyard+by+Marriott+Ciudad+Juarez!5e0!3m2!1sen!2sbd!4v1533791187584"></object>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Map Section End -->
+  
---
+++ index.php
@@ -0,0 +1,358 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords" content="Bootstrap, Landing page, Template, Registration, Landing">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="author" content="Grayrids">
+    <title>Biblioteca Senac</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/line-icons.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/owl.theme.css">
+    <link rel="stylesheet" href="css/nivo-lightbox.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/color-switcher.css">
+    <link rel="stylesheet" href="css/menu_sideslide.css">
+    <link rel="stylesheet" href="css/main.css">    
+    <link rel="stylesheet" href="css/responsive.css">
+
+  </head>
+  
+  <body>
+    <!-- Header Section Start -->
+    <header id="slider-area">  
+      <nav class="navbar navbar-expand-md fixed-top scrolling-navbar bg-white">
+        <div class="container">          
+          <a class="navbar-brand" href="index.php"><span class="lni-bulb"></span>BIBLIOTECA SENAC</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="lni-menu"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto w-100 justify-content-end">
+              <li class="nav-item">
+                <a class="nav-link page-scroll" href="#slider-area">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link page-scroll" href="#perfil">Perfil</a>
+              </li>
+            </ul>              
+          </div>
+        </div>
+      </nav> 
+
+      <!-- Main Carousel Section -->
+      <div id="carousel-area">
+        <div id="carousel-slider" class="carousel slide carousel-fade" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carousel-slider" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-slider" data-slide-to="1"></li>
+            <li data-target="#carousel-slider" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+              <img src="img/slider/bg-1.jpg" alt="">
+              <div class="carousel-caption text-left">
+                <h3 class="wow fadeInRight" data-wow-delay="0.2s">Handcrafted</h1>  
+                <h2 class="wow fadeInRight" data-wow-delay="0.4s">Bootstrap 4 Template</h2>
+                <h4 class="wow fadeInRight" data-wow-delay="0.6s">Comes with All Essential Sections & Elements</h4>
+                <a href="#" class="btn btn-lg btn-common btn-effect wow fadeInRight" data-wow-delay="0.9s">Download</a>
+                <a href="#" class="btn btn-lg btn-border wow fadeInRight" data-wow-delay="1.2s">Get Started!</a>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="img/slider/bg-3.jpg" alt="">
+              <div class="carousel-caption text-center">
+                <h3 class="wow fadeInDown" data-wow-delay="0.3s">Bundled With Tons of</h3>
+                <h2 class="wow bounceIn" data-wow-delay="0.6s">Cutting-edge Features</h2> 
+                <h4 class="wow fadeInUp" data-wow-delay="0.9s">Parallax, Video, Product, Premium Addons and More...</h4>
+                <a href="#" class="btn btn-lg btn-common btn-effect wow fadeInUp" data-wow-delay="1.2s">View Works</a>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="img/slider/bg-2.jpg" alt="">
+              <div class="carousel-caption text-center">
+                <h3 class="wow fadeInDown" data-wow-delay="0.3s">Ready For</h3>
+                <h2 class="wow fadeInRight" data-wow-delay="0.6s">Multi-purpose Websites</h2> 
+                <h4 class="wow fadeInUp" data-wow-delay="0.6s">App, Business, SaaS and Landing Pages</h4>
+                <a href="#" class="btn btn-lg btn-border wow fadeInUp" data-wow-delay="0.9s">Purchase</a>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carousel-slider" role="button" data-slide="prev">
+            <span class="carousel-control" aria-hidden="true"><i class="lni-chevron-left"></i></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carousel-slider" role="button" data-slide="next">
+            <span class="carousel-control" aria-hidden="true"><i class="lni-chevron-right"></i></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>  
+
+    </header>
+    <!-- Header Section End --> 
+
+    <!-- Services Section Start -->
+    <section id="services" class="section">
+      <div class="container">
+        <div class="section-header">          
+          <h2 class="section-title">Nossas categorias</h2>
+          <span>Categorias</span>
+          <p class="section-subtitle">Encontre aqui nossas principais categorias literárias</p>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-md-6 col-xs-12">
+            <div class="item-boxes services-item wow fadeInDown" data-wow-delay="0.2s">
+              <div class="icon color-1">
+                <i class="lni-pencil"></i>
+              </div>
+              <h4>Leituras obrigatórias</h4>
+              <p>Categoria de leituras obrigatórias do Ensino Médio Distrito Criativo, aqui você encontra livros de todas as matérias presentes na matriz curricular e mais.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-xs-12">
+            <div class="item-boxes services-item wow fadeInDown" data-wow-delay="0.4s">
+              <div class="icon color-2">
+                <i class="lni-cog"></i>
+              </div>
+              <h4>Indicados pelo Senac</h4>
+              <p>Aqui você encontra as indicações da nossa escola para os alunos, desde livros clássicos como Romeu e Julieta até os mais modernos, como O Morro dos Ventos Uivantes.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-xs-12">
+            <div class="item-boxes services-item wow fadeInDown" data-wow-delay="0.6s">
+              <div class="icon color-3">
+                <i class="lni-stats-up"></i>
+              </div>
+              <h4>Para ler antes de morrer</h4>
+              <p>Clique aqui para ver nossas indicações de livros que você não pode deixar de ler, dos mais lidos aos merecedores de mais reconhecimento.</p>
+            </div>
+          </div>
+          <!-- Aqui vai ficar o dropdown com as categorias -->
+        </div>
+      </div>
+    </section>
+    <!-- Services Section End -->
+
+    <!-- Blog Section -->
+    <section id="blog" class="section">
+      <!-- Container Starts -->
+      <div class="container">
+        <div class="section-header">          
+          <h2 class="section-title">Blogs</h2>
+          <span>Blogs</span>
+          <p class="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos debitis.</p>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-md-6 col-xs-12 blog-item">
+            <!-- Blog Item Starts -->
+            <div class="blog-item-wrapper">
+              <div class="blog-item-img">
+                <a href="single-post.html">
+                  <img src="img/blog/img1.jpg" alt="">
+                </a>                
+              </div>
+              <div class="blog-item-text"> 
+                <div class="date"><i class="lni-calendar"></i>10 April, 2018</div>
+                <h3><a href="single-post.html">10 Tips to Design a High-converting Landing Page</a></h3>
+                <div class="meta-tags">
+                  <span><a href="#"><i class="lni-eye"></i> 4.5k Views</a></span>
+                  <span><a href="#"><i class="lni-bubble"></i> 07</a></span>
+                  <span><a href="#"><i class="lni-reply"></i> 332</a></span>
+                </div>
+              </div>
+            </div>
+            <!-- Blog Item Wrapper Ends-->
+          </div>
+
+          <div class="col-lg-4 col-md-6 col-xs-12 blog-item">
+            <!-- Blog Item Starts -->
+            <div class="blog-item-wrapper">
+              <div class="blog-item-img">
+                <a href="single-post.html">
+                  <img src="img/blog/img2.jpg" alt="">
+                </a>                
+              </div>
+              <div class="blog-item-text"> 
+                <div class="date"><i class="lni-calendar"></i>10 April, 2018</div>
+                <h3><a href="single-post.html">How to Design a Website For Your App</a></h3>
+                <div class="meta-tags">
+                  <span><a href="#"><i class="lni-eye"></i> 4.5k Views</a></span>
+                  <span><a href="#"><i class="lni-bubble"></i> 07</a></span>
+                  <span><a href="#"><i class="lni-reply"></i> 332</a></span>
+                </div>
+              </div>
+            </div>
+            <!-- Blog Item Wrapper Ends-->
+          </div>
+
+          <div class="col-lg-4 col-md-6 col-xs-12 blog-item">
+            <!-- Blog Item Starts -->
+            <div class="blog-item-wrapper">
+              <div class="blog-item-img">
+                <a href="single-post.html">
+                  <img src="img/blog/img3.jpg" alt="">
+                </a>                
+              </div>
+              <div class="blog-item-text"> 
+                <div class="date"><i class="lni-calendar"></i>10 April, 2018</div>
+                <h3><a href="single-post.html">7 Secretes to Optimize Loading Speed of Your Site</a></h3>
+                <div class="meta-tags">
+                  <span><a href="#"><i class="lni-eye"></i> 4.5k Views</a></span>
+                  <span><a href="#"><i class="lni-bubble"></i> 07</a></span>
+                  <span><a href="#"><i class="lni-reply"></i> 332</a></span>
+                </div>
+              </div>
+            </div>
+            <!-- Blog Item Wrapper Ends-->
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- blog Section End -->
+    
+    <!-- Footer Section Start -->
+    <footer>
+      <!-- Footer Area Start -->
+      <section class="footer-Content">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-mb-12">
+              <h3>Objetivo</h3>
+              <div class="textwidget">
+                <p>Temos como missão, simplificar e melhorar
+                  a qualidade de vida dos estudantes do
+                  Senac de Ensino Médio Distrito Criativo, propondo
+                  livros de leitura obrigatório e também livros
+                  voltados para o lazer.</p>
+              </div>
+              <ul class="footer-social">
+                <li><a class="facebook" href="#"><i class="lni-facebook-filled"></i></a></li>
+                <li><a class="twitter" href="#"><i class="lni-twitter-filled"></i></a></li>
+                <li><a class="linkedin" href="#"><i class="lni-linkedin-fill"></i></a></li>
+                <li><a class="google-plus" href="#"><i class="lni-google-plus"></i></a></li>
+              </ul> 
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-mb-12">
+              <div class="widget">
+                <h3 class="block-title">Acesso rápido</h3>
+                <ul class="menu">
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Perfil</a></li>
+                  <li><a href="#">Sobre nós</a></li>
+                  <li><a href="#">Principais livros</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-mb-12">
+              <div class="widget">
+                <h3 class="block-title">Entre em contato</h3>
+                <ul class="contact-footer">
+                  <li>
+                    <strong>Endereço :</strong> <span>Cristóvão Colombo, 545 - Bairro Floresta | Porto Alegre - RS</span>
+                  </li>
+                  <li>
+                    <strong>Telefone :</strong> <span>(51) 99999-9999</span>
+                  </li>
+                  <li>
+                    <strong>E-mail :</strong> <span><a href="#">contato@paulindos.com</a></span>
+                  </li>
+                </ul> 
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-mb-12">
+              <div class="widget">
+                <h3 class="block-title">Instagram</h3>
+                <ul class="instagram-footer">
+                  <li><a href="#"><img src="img/instagram/insta1.jpg" alt=""></a></li>
+                  <li><a href="#"><img src="img/instagram/insta2.jpg" alt=""></a></li>
+                  <li><a href="#"><img src="img/instagram/insta3.jpg" alt=""></a></li>
+                  <li><a href="#"><img src="img/instagram/insta4.jpg" alt=""></a></li>
+                  <li><a href="#"><img src="img/instagram/insta5.jpg" alt=""></a></li>
+                  <li><a href="#"><img src="img/instagram/insta6.jpg" alt=""></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Footer area End -->
+      
+      <!-- Copyright Start  -->
+      <div id="copyright">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="site-info float-left">
+                <p>Desenvolvido por <a href="https://github.com/senacrs-emti/2022-3M-biblioteca" rel="nofollow">Equipe Paulindos</a></p>
+              </div>              
+              <div class="float-right">  
+                <ul class="nav nav-inline">
+                  <li class="nav-item">
+                    <a class="nav-link active" href="#">About Prime</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">TOS</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Return Policy</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">FAQ</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Copyright End -->
+
+    </footer>
+    <!-- Footer Section End --> 
+
+    <!-- Go To Top Link -->
+    <a href="#" class="back-to-top">
+      <i class="lni-arrow-up"></i>
+    </a>
+
+    <div id="loader">
+      <div class="spinner">
+        <div class="double-bounce1"></div>
+        <div class="double-bounce2"></div>
+      </div>
+    </div>    
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="js/jquery-min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/color-switcher.js"></script>
+    <script src="js/jquery.mixitup.js"></script>
+    <script src="js/nivo-lightbox.js"></script>
+    <script src="js/owl.carousel.js"></script>    
+    <script src="js/jquery.stellar.min.js"></script>    
+    <script src="js/jquery.nav.js"></script>    
+    <script src="js/scrolling-nav.js"></script>    
+    <script src="js/jquery.easing.min.js"></script>     
+    <script src="js/wow.js"></script> 
+    <script src="js/jquery.vide.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>    
+    <script src="js/jquery.magnific-popup.min.js"></script>    
+    <script src="js/waypoints.min.js"></script>    
+    <script src="js/form-validator.min.js"></script>
+    <script src="js/contact-form-script.js"></script>   
+    <script src="js/main.js"></script>
+    
+  </body>
+</html>
